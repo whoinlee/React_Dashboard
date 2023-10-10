7b2696a00540ababde102ff9969222ca956ea51f
WhoIN Lee <whoinlee@gmail.com>
2023-10-10 19:16:19 -0400

    PieChart trimmed

--- src/components/charts/PieChart.jsx
+++ src/components/charts/PieChart.jsx
@@ -50,7 +50,7 @@ const PieChart = () => {
       arcLinkLabelsTextColor={colors.grey[100]}
       arcLinkLabelsThickness={2}
       arcLinkLabelsColor={{ from: "color" }}
-      enableArcLabels={false}
+      enableArcLabels={true}
       arcLabelsRadiusOffset={0.4}
       arcLabelsSkipAngle={7}
       arcLabelsTextColor={{
